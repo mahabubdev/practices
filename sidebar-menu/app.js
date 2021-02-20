79386de12065b3249e02152d41b4a947a0a1773b
Md Mahabub <mahabub200bd@gmail.com>
2021-02-20 17:41:30 +0600

    portfolio-ui-added

--- sidebar-menu/app.js
+++ sidebar-menu/app.js
@@ -10,6 +10,7 @@
      var nav = document.getElementById(navId);
      var bdyWrap = document.getElementById(bdyId);
      var header = document.getElementById(hId);
+     
 
      // toggle classes
      tog.addEventListener('click', () => {
@@ -19,20 +20,42 @@
         tog.classList.toggle('lni-close');
         bdyWrap.classList.toggle('bdy-pd');
         header.classList.toggle('bdy-pd');
+        nav_links.forEach(ll => ll.classList.toggle('link-block'))
     });
  };
-
+ 
  showNavbar('toggle', 'navbar', 'bdy-wrapper', 'header');
 
 
  /*=== Link active ===*/
- const linkColor = document.querySelectorAll('.nav_link');
+ const nav_links = document.querySelectorAll('.nav_link');
  function colorLink() {
-     if (linkColor) {
-         linkColor.forEach(l => l.classList.remove('active'))
+     if (nav_links) {
+         nav_links.forEach(l => l.classList.remove('active'))
          this.classList.add('active');
      }
  }
- linkColor.forEach(l => l.addEventListener('click', colorLink));
+ nav_links.forEach(l => l.addEventListener('click', colorLink));
+
+
+
+
+
+/*=== Sub-Menu ===*/
+const hasMenu = document.querySelectorAll('.has_drop');
+function subMenu() {
+    if (hasMenu) {
+        hasMenu.forEach(hm => {
+            const chevron_down = document.createElement('span');
+            chevron_down.classList.add('lni');
+            chevron_down.classList.add('lni-chevron-down');
+            hm.appendChild(chevron_down);
+        });
+    }
+}
+subMenu();  //called
+
+
+
 
  /*=== END ===*/
